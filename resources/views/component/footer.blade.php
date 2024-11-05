71e4ece974a840f676eaf39a968af0a0a26e43a9
Bramasta <bramastasaka9@gmail.com>
2024-11-05 12:52:34 +0700

    Update footer.blade.php

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,7 +1,7 @@
    <div class="nk-footer">
        <div class="container-xl wide-xl">
            <div class="nk-footer-wrap">
-               <div class="nk-footer-copyright"> &copy; SI PENGAMPU. Politeknik Negeri Jember 
+               <div class="nk-footer-copyright"> &copy; TEACH MAP. Politeknik Negeri Jember 
                </div>
                
            </div>
